1ae3ae6d3542651eb172cbe568766f54567f2e4f
luizguerra <luizpedrofg@gmail.com>
2020-03-17 14:45:34 -0300

    Replaced the files to MVC

---
+++ nsmoviedb/Controller/MovieDetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  MovieDetailsViewController.h
+//  nsmoviedb
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 17/03/20.
+//  Copyright © 2020 Jobe Diego Dylbas dos Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieDetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ nsmoviedb/Controller/MovieDetailsViewController.m
@@ -0,0 +1,32 @@
+//
+//  MovieDetailsViewController.m
+//  nsmoviedb
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 17/03/20.
+//  Copyright © 2020 Jobe Diego Dylbas dos Santos. All rights reserved.
+//
+
+#import "MovieDetailsViewController.h"
+
+@interface MovieDetailsViewController ()
+
+@end
+
+@implementation MovieDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
