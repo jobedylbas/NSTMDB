431a53d15b7a8e8375f10b4bf8f6c28e15a51fed
Jobe Dylbas <jobe.dylbas@inf.ufrgs.br>
2020-03-17 13:28:21 -0300

    Network - Movie Posters Request

--- nsmoviedb/Network/Network.m
+++ nsmoviedb/Network/Network.m
@@ -31,6 +31,23 @@
 //    NSLog(@"data: %@",jsonObject);
 }
 
++ (void) makePosterRequest: (NSURL*) url completion:(void (^)(NSData *, NSError *))completionBlock {
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+    {
+        //Create a block to handle the background thread in the dispatch method.
+        void (^runAfterCompletion)(void) = ^void (void) {
+            if (error) {
+                completionBlock (nil, error);
+            } else {
+                completionBlock(data, error);
+            }
+        };
+        //Dispatch the queue
+        dispatch_async(dispatch_get_main_queue(), runAfterCompletion);
+       }];
+        [task resume]; // to start the download task
+//    NSLog(@"data: %@",jsonObject);
+}
 
 - (void) reqPopularMovies {
     static NSString* key = @"fb61737ab2cdee1c07a947778f249e7d";
@@ -49,7 +66,6 @@
 }
 
 - (void) reqNowPlayingMovies {
-    
     static NSString* key = @"fb61737ab2cdee1c07a947778f249e7d";
     static NSString* nowPlayingURL = @"https://api.themoviedb.org/3/movie/now_playing?";
     NSURL *url = [[NSURL alloc]initWithString: [NSString stringWithFormat:@"%@%@", nowPlayingURL, key]];
@@ -64,5 +80,23 @@
                       }
                   }];
 }
+
+- (void) reqMoviePoster:(NSString *)movieId {
+    static NSString *key = @"fb61737ab2cdee1c07a947778f249e7d";
+    static NSString *keyAttr = @"/images?api_key=";
+    static NSString *prefix = @"https://api.themoviedb.org/3/movie/";
+    NSURL *url = [[NSURL alloc]initWithString: [NSString stringWithFormat:@"%@%@%@%@", prefix, movieId, keyAttr, key]];
+    
+     [Network makePosterRequest: url
+                  completion: ^(NSData *data, NSError *error) {
+                      if (error) {
+                          self.errorMessage = [error localizedDescription];
+                      } else {
+                          self.image = data;
+//                          NSLog(@"%@", self.response);
+                      }
+                  }];
+}
+
 
 @end
