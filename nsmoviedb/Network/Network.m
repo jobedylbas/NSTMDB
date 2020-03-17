dd26242fd12e62e27eb20b52732533d50f4e06e0
Jobe Dylbas <jobe.dylbas@inf.ufrgs.br>
2020-03-16 22:47:04 -0300

    Making Request to popularmovies

--- nsmoviedb/Network/Network.m
+++ nsmoviedb/Network/Network.m
@@ -14,26 +14,29 @@
 @implementation Network
 
 - (void) reqPopularMovies {
-    static NSString* key = @"fb61737ab2cdee1c07a947778f249e7";
-    static NSString* popularURL = @"https://api.themoviedb.org/3/movie/popular?";
-    printf("Network before call");
-    NSString *url = [NSString stringWithFormat:@"%@%@", key, popularURL];
+    static NSString* key = @"fb61737ab2cdee1c07a947778f249e7d";
+    static NSString* popularURL = @"https://api.themoviedb.org/3/movie/popular?api_key=";
+    NSString *_url = [NSString stringWithFormat:@"%@%@", popularURL, key];
+    NSLog(@"%@", _url);
 
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    [request setHTTPMethod:@"GET"];
-    [request setURL:[NSURL URLWithString:url]];
-
-    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:
-      ^(NSData * _Nullable data,
-        NSURLResponse * _Nullable response,
-        NSError * _Nullable error) {
-
-          NSString *myString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-          NSLog(@"Data received: %@", myString);
-    }] resume];
+    NSURL *url = [[NSURL alloc]initWithString:_url];
+    //type your URL u can use initWithFormat for placeholders
+//    NSURLSession *session = [NSURLSession sharedSession];  //use NSURLSession class
+//    NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url];
+    //You then can use NSURLSessionDownloadTask or NSURLSessionUploadTask
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+    {
+        NSDictionary *jsonObject=[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+//        [self loadDataFromDictionary:(NSArray*)jsonObject];
+        NSLog(@"data: %@",jsonObject);
+       }];
+        [task resume]; // to start the download task
 }
 
 - (NSString *) reqNowPlayingMovies {
+    NSLog(@"hello from NSLog");
+
     return @"lola";
 }
 
