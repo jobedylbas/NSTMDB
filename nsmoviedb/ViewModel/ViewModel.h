02f8d35b527907e6452fc2414a58b341e5dd5e5a
Jobe Dylbas <jobe.dylbas@inf.ufrgs.br>
2020-03-23 15:32:29 -0300

    Stable of ViewModel

---
+++ nsmoviedb/ViewModel/ViewModel.h
@@ -0,0 +1,13 @@
+//
+//  ViewModel.h
+//  nsmoviedb
+//
+//  Created by Jobe Diego Dylbas dos Santos on 23/03/20.
+//  Copyright © 2020 Jobe Diego Dylbas dos Santos. All rights reserved.
+//
+
+#ifndef ViewModel_h
+#define ViewModel_h
+
+
+#endif /* ViewModel_h */
---
+++ nsmoviedb/ViewModel/ViewModel.m
@@ -0,0 +1,9 @@
+//
+//  ViewModel.m
+//  nsmoviedb
+//
+//  Created by Jobe Diego Dylbas dos Santos on 23/03/20.
+//  Copyright © 2020 Jobe Diego Dylbas dos Santos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
